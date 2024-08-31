a7d2ec95dcc3c9e671334bcd87a869596c90e0d2
Saipraneeth Mukku <saipmukku@gmail.com>
2024-08-31 19:41:18 -0400

    Adding the React project template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Neeth's Portfolio Website Placeholder
+      </header>
+    </div>
+  );
+}
+
+export default App;
