1010bf243d9f40ac59e89c3e2255b74f3aff57b6
Saipraneeth Mukku <saipmukku@gmail.com>
2024-10-09 21:49:18 -0400

    Added message functionality, but commented it out. Also included some other small updates

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Box, Avatar, Tooltip, Fade, Link } from '@mui/material';
+import { Box, Avatar, Tooltip, Fade, Link, Modal, TextField, Button, Typography } from '@mui/material';
 import { motion } from "framer-motion";
-import EmailIcon from '@mui/icons-material/Email';
+import EmailTwoToneIcon from '@mui/icons-material/EmailTwoTone';
+import MessageTwoToneIcon from '@mui/icons-material/MessageTwoTone';
 import '../App.css'
 
 function ContactPage() {
@@ -17,9 +18,12 @@ function ContactPage() {
 
   return (
     <Box className="outer-contact">
+      <Box className="message-contact">
+        {/* <DeprecatedMessageFunction /> */}
+      </Box>
       <Box>
         <Tooltip title="Leo" placement="top" arrow TransitionComponent={Fade}
-          slotProps={{ popper: { modifiers: { name: "offset", options: { offset: [0, -90]} } } }}>
+          slotProps={{ popper: { modifiers: { name: "offset", options: { offset: [0, -75]} } } }}>
           <motion.div className="animated-avatar" whileHover={{ scale: 1.02 }} whileTap={{ scale: 1.05 }}>
             <Avatar alt="Neeth's picture" src="leo.png" sx={{ height: 420, width: 420 }} variant="circle"/>
           </motion.div>
@@ -29,7 +33,7 @@ function ContactPage() {
             <Tooltip title={emailTooltip} placement="top" arrow TransitionComponent={Fade}
               slotProps={{ popper: { modifiers: { name: "offset", options: { offset: [0, -5]} } } }}>
               <motion.div className="animated-icon" whileHover={{ scale: 1.15 }} whileTap={{ scale: 1.35 }}>
-                <EmailIcon sx={{ width: "64px", height: "64px" }} onClick={copyAndUpdateTooltip}/>
+                <EmailTwoToneIcon sx={{ width: "64px", height: "64px" }} onClick={copyAndUpdateTooltip}/>
               </motion.div>
             </Tooltip>
           </Box>
@@ -53,6 +57,44 @@ function ContactPage() {
           </Box>
         </Box>
       </Box>
+      <Box className="message-contact">
+
+      </Box>
+    </Box>
+  )
+}
+
+function DeprecatedMessageFunction() {
+  const [messagePrompt, setMessagePrompt] = useState(false);
+
+  const promptMessage = () => {
+    setMessagePrompt(!messagePrompt);
+  }
+
+  return (
+    <Box className="message-contact">
+      <motion.div whileHover={{ scale: 1.25 }} whileTap={{ scale: 1.5 }}>
+        <MessageTwoToneIcon sx={{ width: "64px", height: "64px" }} onClick={promptMessage} />
+      </motion.div>
+      <Modal className="message-modal" open={messagePrompt}>
+        <Box className="modal-box">
+          <Typography variant="h4" sx={{ textAlign: 'center' }}>Type a message below and click send to send me an inquiry.</Typography>
+          <Typography variant="h5" sx={{ textAlign: 'center' }}>Please include your email if you would like a reply.</Typography>
+          <TextField className="message" variant="filled" color="#000000" label="Enter Message" multiline rows={8} fullWidth />
+          <Box className="modal-buttons">
+            <motion.div whileHover={{ scale: 1.25 }} whileTap={{ scale: 1.5 }}>
+              <Button onClick={promptMessage}>
+                Send
+              </Button>
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.25 }} whileTap={{ scale: 1.5 }}>
+              <Button onClick={promptMessage}>
+                Cancel
+              </Button>
+            </motion.div>
+          </Box>
+        </Box>
+      </Modal>
     </Box>
   )
 }
